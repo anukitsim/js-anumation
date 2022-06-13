ea91d9899b9b02eb9a77594a987c3b22b297dff1
anukitsim <104069419+anukitsim@users.noreply.github.com>
2022-06-13 20:57:56 +0400

    done.

---
+++ animated.js
@@ -0,0 +1,21 @@
+let windo = document.getElementById("window");
+let day = document.getElementById("day");
+let night = document.getElementById("night");
+let sunset = document.getElementById("sunset");
+
+day.addEventListener("click", function(){
+    windo.style.backgroundColor = "#87CEEB"
+});
+
+night.addEventListener("click", function(){
+    windo.style.backgroundColor = "#08183A"
+});
+
+sunset.addEventListener("click", function(){
+    windo.style.backgroundColor = "#FC6A38"
+});
+
+
+
+
+
