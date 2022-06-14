9bff82c18115428fcdb1f78156d6172845b89849
anukitsim <104069419+anukitsim@users.noreply.github.com>
2022-06-14 11:25:03 +0400

    done

---
+++ animated2.js
@@ -0,0 +1,8 @@
+document.getElementById("neon").onclick = function (){
+    neonMouse ()
+};
+
+function neonMouse (){
+    var neon = document.getElementById("neon")
+    neon.classList.add("start-rotation");
+}
